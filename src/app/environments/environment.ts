843a09cd9d95373fd0e7b9d10d319e39b626c557
mohammed-rehaan <mohammedrehaan08@gmail.com>
2025-07-07 23:32:05 +0530

    - Final commit for today

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = 
 {
   production: false,
-   baseUrl: 'https://localhost:7203/'
+  //  baseUrl: 'https://localhost:7203/'
 
 
  // baseUrl: ' https://b478-2405-201-d026-20f5-8843-81b8-dc57-97ad.ngrok-free.app/'
@@ -23,6 +23,6 @@ export const environment =
   //baseUrl: 'https://ef1b-152-58-195-160.ngrok-free.app/'
   // baseUrl: 'https://ef1b-152-58-195-160.ngrok-free.app/'  //  baseUrl:"https://7883-2405-201-d026-20f5-b88f-2896-cf70-df22.ngrok-free.app/"
        
- // baseUrl: "https://9c94-2405-201-d026-20f5-2921-a716-c562-2cc7.ngrok-free.app/"
+ baseUrl: "https://9c94-2405-201-d026-20f5-2921-a716-c562-2cc7.ngrok-free.app/"
   // baseUrl: 'https://pm.api.race-elearn.com:5005/'
 };
